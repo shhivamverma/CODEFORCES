30eacc5dfedffca3e7ad848efb786fc445c41cf1
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-09-05 15:23:45 +0530

    Add files via upload

---
+++ codeforces4.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class codeforces4
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        int c=0;
+        for(int i=1;i<=t;i++)
+        {
+            int n=sc.nextInt();
+            if((n%15)+1<3)
+            {
+                c=(n/15)*3+((n%15)+1);
+            }
+            else
+            {
+                c=(n/15)*3+3;
+            }
+            System.out.println(c);
+        }
+    }
+}
