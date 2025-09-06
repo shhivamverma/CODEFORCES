1d2981df1733a5f000dec5be3db0d1dc3d222260
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-09-07 03:39:01 +0530

    Add files via upload

---
+++ codeforces5.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class codeforces5
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=1;i<=t;i++)
+        {
+        int n=sc.nextInt();
+        int x=sc.nextInt();
+        int status[]=new int[n];
+        for(int m=0;m<n;m++)
+        {
+            status[m]=sc.nextInt();
+        }
+        int first=-1;
+        int last=-1;
+        for (int j = 0; j < n; j++) 
+        {
+            if (status[j] == 1)
+            {
+                if(first==-1)
+                {
+                    first=j;        
+                }
+                last=j;
+            }
+        }
+            int range = last-first+1;
+            if(x>=range)
+            {
+                System.out.println("YES");
+            }
+            else
+            {
+                System.out.println("NO");
+            }
+        }
+    }
+}
+
