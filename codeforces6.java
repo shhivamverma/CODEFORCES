bbfb6a9efb302dec18a30537ee66a2ea6301f0d5
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-09-07 17:08:06 +0530

    Add files via upload

---
+++ codeforces6.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class codeforces6
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String arr[]=new String[n];
+        for(int j=0;j<n;j++)
+        {
+            arr[j]=sc.next();
+        }
+        int grp=1;
+        for(int i=1;i<n;i++)
+        {
+            if (!arr[i].equals(arr[i - 1]))
+            {
+                grp++;    
+            }
+        }
+        System.out.println(grp);
+    }
+}
