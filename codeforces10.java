04d1335ed49fbf5ceeb114b33247eae4f243fff2
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-09-10 23:38:43 +0530

    Add files via upload

---
+++ codeforces10.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class codeforces10
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int t = sc.nextInt();   
+        for(int i=1;i<=t;i++)
+        {
+            int n = sc.nextInt();   
+            int[] a = new int[n];
+            int[] b = new int[n];
+            for (int j=0;j<n;j++)
+            {
+                a[j]=sc.nextInt();
+            }
+            for (int m=0;m<n;m++) 
+            {
+                b[m]=sc.nextInt();
+            }
+            int ans=0;
+            for(int k=0;k<n;k++)
+            {
+                if(a[k]>b[k])
+                {
+                    ans=ans+(a[k]-b[k]);
+                }
+            }
+            ans++;
+            System.out.println(ans);
+        }
+    }
+}
