4b7f83cab66a225ab3a5732e1e5d5a07a9fd984b
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-09-11 22:08:43 +0530

    Add files via upload

---
+++ codeforces11.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class codeforces11
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=1;i<=t;i++)
+        {
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            int p=sc.nextInt();
+            int max=n*p;
+            int min=-max;
+            if(k<min || k>max)
+            {
+                System.out.println("-1");
+            }
+            else
+            {
+                int mod=Math.abs(k);
+                int operations=(mod+p-1)/p;
+                System.out.println(operations);
+            }
+        }
+    }
+}
