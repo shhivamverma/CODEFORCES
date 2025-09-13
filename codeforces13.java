cb8d885aca048cb8b9617d4c1aa57350e13a017f
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-09-13 21:27:39 +0530

    Add files via upload

---
+++ codeforces13.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class codeforces13
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=1;i<=t;i++)
+        {
+            int x1=sc.nextInt();
+            int y1=sc.nextInt();
+            int x2=sc.nextInt();
+            int y2=sc.nextInt();
+            int diff_x=x2-x1;
+            int diff_y=y2-y1;
+            if(diff_y<0 || diff_y-diff_x<0)
+            {
+                System.out.println("-1");
+            }
+            else
+            {
+                int totalmoves=2*diff_y-diff_x;
+                System.out.println(totalmoves);
+            }
+        }
+    }
+}
