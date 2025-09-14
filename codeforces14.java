041976c9aa88f931d85bd5e099b956894aa606fe
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-09-14 16:22:08 +0530

    Add files via upload

---
+++ codeforces14.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class codeforces14
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=1;i<=t;i++)
+        {
+            int n=sc.nextInt();
+            int a[]=new int[n];
+            int k=sc.nextInt();
+            for(int j=0;j<n;j++)
+            {
+                a[j]=sc.nextInt();
+            }
+            int good_days=0;
+            int count=0;
+            for(int m=0;m<n;m++)
+            {
+                if(a[m]==0)
+                {
+                    good_days++;
+                    if(good_days==k)
+                    {
+                        count++;
+                        good_days=0;
+                        m++;
+                    }
+                }
+                else
+                {
+                    good_days=0;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
