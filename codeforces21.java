6588ef7cc8c16649e2dff3d43706f27961e76fe4
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-09-21 15:36:38 +0530

    Update codeforces21.java

--- codeforces21.java
+++ codeforces21.java
@@ -1,3 +1,50 @@
-public class codeforces21 {
+import java.util.*;
+public class codeforces21 
+{                                                                                         // Task 1 : Sereja and Dima
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(); 
+        int cards[]=new int[n];
+        for (int i=0;i<n;i++) 
+        {
+            cards[i]=sc.nextInt();
+        }
+        int serejaScore=0;
+        int dimaScore=0;
+        int left=0;
+        int right=n-1;
+        boolean serejaTurn=true;
+        while(left <= right) 
+        {
+            if(cards[left] > cards[right]) 
+            {
+                if(serejaTurn) 
+                {
+                    serejaScore += cards[left];
+                } 
+                else 
+                {
+                    dimaScore += cards[left];
+                }
+                left++;
+            } 
+            else 
+            {
+                if (serejaTurn) 
+                {
+                    serejaScore += cards[right];
+                } 
+                else 
+                {
+                    dimaScore += cards[right];
+                }
+                right--;
+            }
+            serejaTurn = !serejaTurn;
+        }
 
+        System.out.println(serejaScore + " " + dimaScore);
+    }
 }
+
