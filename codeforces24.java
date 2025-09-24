0c091f8b2798fb6b6f28ea8f37d06636d6d247c4
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-09-24 19:08:12 +0530

    Update codeforces24.java

--- codeforces24.java
+++ codeforces24.java
@@ -1,17 +1,21 @@
 import java.util.*;
-public class codeforces24 
-{
-    public static void main(String[] args) 
-    {
+                                                                                    // Problem 1814A – Coins
+public class codeforces24 {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();   
-        while (t != 0) 
-        {
-            int n = sc.nextInt();  
-            int ans = (n + 1) / 2; 
-            System.out.println(ans);
-            t--;
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long n = sc.nextLong();
+            long k = sc.nextLong();
+
+            if (n % 2 == 0) {
+                System.out.println("YES");
+            } else if (k % 2 == 1 && n >= k) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
         }
+        sc.close();
     }
 }
-
