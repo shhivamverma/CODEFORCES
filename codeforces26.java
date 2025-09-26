5a854d09f3282505fe5a5901a9293a67e5b17d0d
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-09-26 12:58:35 +0530

    Update codeforces26.java

--- codeforces26.java
+++ codeforces26.java
@@ -1,3 +1,33 @@
-public class codeforces26 {
+import java.util.*;
+public class codeforces26 
+{                                                                           // Problem - 1303 Erasing Zeroes
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();        
+        while (t!=0) 
+        {
+            String s = sc.next();    
+            int first = s.indexOf('1');
+            int last = s.lastIndexOf('1');
 
+            if (first == -1 || first == last) 
+            {
+                System.out.println(0);
+            } 
+            else 
+            {
+                int count = 0;
+                for (int i = first; i <= last; i++) 
+                {
+                    if (s.charAt(i) == '0') 
+                    {
+                        count++;
+                    }
+                }
+                System.out.println(count);
+            }
+            t--;
+        }
+    }
 }
