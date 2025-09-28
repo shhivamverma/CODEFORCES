6ad04fdf0f81ec309fb659d49e8dd70db2405373
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-09-28 20:36:59 +0530

    Update codeforces28.java

--- codeforces28.java
+++ codeforces28.java
@@ -1,3 +1,17 @@
-public class codeforces28 {
-
+import java.util.*;
+public class codeforces28                                   // 1987A — “Upload More RAM"
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();   
+        while (t!=0) 
+        {
+            long n = sc.nextLong();    
+            long k = sc.nextLong();    
+            long ans = 1 + (n - 1) * k; 
+            System.out.println(ans);
+            t--;
+        }
+    }
 }
