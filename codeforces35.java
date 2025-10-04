af4b53c5d58a33838622e6ed317f4c30cba08869
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-10-04 21:12:58 +0530

    Update codeforces35.java

--- codeforces35.java
+++ codeforces35.java
@@ -1,3 +1,30 @@
-public class codeforces35 {
-
+import java.util.*;
+public class codeforces35                                   // Problem 1900A - Cover In Water
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt(); 
+        for(int i=1;i<=t;i++)
+        {
+            int n=sc.nextInt(); 
+            String s=sc.next();
+            if (s.contains("...")) 
+            {
+                System.out.println(2);
+            } 
+            else 
+            {
+                int count=0;
+                for(int j=0;j<s.length();j++) 
+                {
+                    if(s.charAt(j)=='.')
+                    { 
+                        count++;
+                    }
+                }
+                System.out.println(count);
+            }
+        }
+    }
 }
