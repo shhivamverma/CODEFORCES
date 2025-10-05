ea0ccdd5e25acd29b8b86ca5c99f637a485c3e91
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-10-05 18:55:46 +0530

    Add files via upload

---
+++ codeforces36.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class codeforces36 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();  // number of test cases
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int a[] = new int[n];
+            
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            // If only one element, first player always wins
+            if (n == 1) {
+                System.out.println("YES");
+                System.out.println(1);
+                continue;
+            }
+
+            int freq[] = new int[k];  // store how many have same remainder mod k
+            for (int i = 0; i < n; i++) {
+                freq[a[i] % k]++;
+            }
+
+            int ans = -1;
+            for (int i = 0; i < n; i++) {
+                if (freq[a[i] % k] == 1) {  // unique remainder
+                    ans = i + 1;  // 1-based index
+                    break;
+                }
+            }
+
+            if (ans == -1) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+                System.out.println(ans);
+            }
+        }
+
+        sc.close();
+    }
+}
