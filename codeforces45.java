c063d079e1a0940486d01ded8cf37f7cb0804356
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-10-12 21:40:30 +0530

    Update codeforces45.java

--- codeforces45.java
+++ codeforces45.java
@@ -1,3 +1,22 @@
-public class codeforces45 {
-
+import java.util.*;
+public class codeforces45                          // Problem 467A - George and Accomodation 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int c=0;
+        for(int i=1;i<=n;i++)
+        {
+            int p=sc.nextInt();
+            int q=sc.nextInt();
+            {
+                if((q-p)>=2)
+                {
+                   c++;
+                }
+            }
+        }
+        System.out.println(c);
+    }
 }
