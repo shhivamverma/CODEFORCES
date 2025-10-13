2643ead2fd91f8884e3121bcdc23beddcb3fd848
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-10-13 15:49:33 +0530

    Add files via upload

---
+++ codeforces47.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class codeforces47
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int j=1;j<=n;j++)
+        {
+            if(j%2!=0)
+            {
+                if(j==n)
+                {
+                    System.out.print("I hate"+" ");
+                    break;
+                }
+                System.out.print("I hate"+" "+"that"+" ");
+               
+            }
+            else
+            {
+                if(j==n)
+                {
+                    System.out.print("I love"+" ");
+                    break;
+                }
+            
+            
+                System.out.print("I love"+" "+"that"+" ");
+            
+        }
+    }
+        System.out.print("it");
+    }
+}
