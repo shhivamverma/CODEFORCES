d690e1ba1b61ea8c7deaaf733d6af5870ee5a9bc
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-10-16 00:08:32 +0530

    Add files via upload

---
+++ codeforces50.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class codeforces50
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        int res=m*n;
+        System.out.println(res/2);
+    }
+}
