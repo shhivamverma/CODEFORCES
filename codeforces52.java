93279bc5fd70af7e0b0f1286f70483de213a9c99
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-10-18 12:00:20 +0530

    Add files via upload

---
+++ codeforces52.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class codeforces52
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+       for (int i = 0; i < n; i++)
+       {
+          int a=sc.nextInt();
+          int b=sc.nextInt();
+          int c=sc.nextInt();
+          if (a < b && b < c)
+          {
+              System.out.println("STAIR");
+          }
+          else if (a < b && b > c)
+         {
+             System.out.println("PEAK");
+         }
+         else
+         {
+             System.out.println("NONE");
+         }
+       }
+    }
+}
