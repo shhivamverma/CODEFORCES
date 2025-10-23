5b38cf0db8ec29b1a1f3d1fc7a4caaae1ab259ae
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-10-24 00:57:15 +0530

    Add files via upload

---
+++ codeforces58.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class codeforces58
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in); 
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int rem=0, days=0;
+        if (a >= b)
+        {
+           days = b;
+           rem = a - days;
+        }
+        else
+        {
+           days = a;
+           rem = b - days;
+        }
+        System.out.println(days);
+        System.out.println(rem/2);
+    }
+}
