c7c7846486ce3e4dbab8d66ff67b3576b5ab910e
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-10-23 13:05:52 +0530

    Add files via upload

---
+++ codeforces57.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class codeforces57
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+    
+    double total = 0;
+    int n=sc.nextInt();
+    for (int i = 1; i <= n; i++)
+    {
+        int a=sc.nextInt();
+        total += a / 100.0;
+    }
+
+    double percent = (total / n) * 100;
+    System.out.println(percent);
+}
+}
