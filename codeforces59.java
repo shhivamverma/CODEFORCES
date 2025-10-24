0d8fa6154e8c2dd55c4b6263a37b8316857349d9
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-10-25 00:01:31 +0530

    Update codeforces59
    Implement logic to find the minimum multiplier for k that results in a last digit of r or 0.

--- codeforces59.java
+++ codeforces59.java
@@ -1,3 +1,21 @@
-public class codeforces59 {
-
+import java.util.*;
+public class codeforces59
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int k=sc.nextInt();
+        int r=sc.nextInt();
+        for (int i = 1; i <= 15; i++)
+        {
+            int a = (k * i) % 10;
+            if (a == r || a==0)
+            {
+                 System.out.println(i);
+                 break;
+            }
+        }
+    }
 }
+
+    
