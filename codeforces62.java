b646e09197c816422e02a895218d52354a0a9cb0
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-10-27 10:38:43 +0530

    Add files via upload

---
+++ codeforces62.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class codeforces62
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            int a=sc.nextInt();
+            System.out.println(2*a-2);
+        }
+    }
+}
