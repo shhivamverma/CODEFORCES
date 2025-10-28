2bb746a44d72c2f86b3cd23f42bb935ee968d874
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-10-28 23:28:24 +0530

    Add files via upload

---
+++ codeforces64.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class codeforces64
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();
+        for (int i = 1; i <= q; i++) 
+        {
+            int n = sc.nextInt();
+            String s = sc.next();
+            String t = sc.next();
+            boolean possible = true;
+            for (char ch = 'a'; ch <= 'z'; ch++) 
+            {
+                int count1 = 0;
+                int count2 = 0;
+                for (int j = 0; j < n; j++) 
+                {
+                    if (s.charAt(j) == ch)
+                    {
+                        count1++;
+                    }
+                }
+                for (int j = 0; j < n; j++) 
+                {
+                    if (t.charAt(j) == ch)
+                    {
+                        count2++;
+                    }
+                }
+                if (count1 != count2) 
+                {
+                    possible = false;
+                    break;
+                }
+            }
+
+            if (possible)
+            {
+                System.out.println("YES");
+            }
+            else
+            {
+                System.out.println("NO");
+            }
+        }
+        
+    }
+}
