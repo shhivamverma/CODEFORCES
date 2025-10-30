40b99df8b34c804c7948b0754c71fda5b868f753
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-10-30 20:51:44 +0530

    Add files via upload

---
+++ codeforces66.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class codeforces66
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        int count1=0;
+        int count2=0;
+        for(int i=1;i<=n;i++)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            if(a>b)
+            {
+               count1++;
+            }
+            if(b>a)
+            {
+               count2++;
+            }
+        }
+        if(count1>count2)
+        {
+            System.out.println("Mishka");
+        }
+        else if(count2>count1)
+        {
+            System.out.println("Chris");
+        }
+        else
+        {
+            System.out.println("Friendship is magic!^^");
+        }
+    }
+}
