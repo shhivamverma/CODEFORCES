e3debf9a7894a6127b3f9be4d25e7087abe65701
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-11-02 18:37:52 +0530

    Add files via upload

---
+++ codeforces69.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class codeforces69
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        if (num%2==0 && num>2)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
