a105c887892b22465a5d5bdcbc1c0e63e92ee1ab
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-11-05 16:32:42 +0530

    Update codeforces72.java
    Added a comment to indicate the problem number.

--- codeforces72.java
+++ codeforces72.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class codeforces72
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)               // Problem 1877A - Goals of Victory
     {
 		Scanner sc=new Scanner(System.in);
 		int t=sc.nextInt(); 
@@ -21,4 +21,5 @@ public class codeforces72
 			System.out.println(sum*-1); 
 		}
     }
+
 }
