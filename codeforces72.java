12e8c00c4c3b0ed1a07144e5671814eba15aa9e1
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-11-05 16:31:42 +0530

    Add files via upload

---
+++ codeforces72.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class codeforces72
+{
+	public static void main(String[] args) 
+    {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt(); 
+		for(int i=1;i<=t;i++) 
+        { 
+			int n = sc.nextInt(); 
+			int arr[]=new int[n-1];
+			for (int j=0;j<n-1;j++)
+            { 
+				arr[j] = sc.nextInt(); 
+			}
+            int sum=0;
+			for (int m=0;m<n-1;m++) 
+            {
+				sum=sum+arr[m]; 
+			}
+			System.out.println(sum*-1); 
+		}
+    }
+}
