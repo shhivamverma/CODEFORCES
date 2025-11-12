76c3b1a4d19e040c93d2dd4f24156afc62ac4dc6
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-11-12 21:32:17 +0530

    Update codeforces89.java
    Added a comment indicating the problem number for clarity.

--- codeforces89.java
+++ codeforces89.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class codeforces89
 {
-    public static void main(String args[])
+    public static void main(String args[])            // Problem 1853A - Desorting
     {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
@@ -43,4 +43,5 @@ public class codeforces89
             }
         }
     }
+
 }
