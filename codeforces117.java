768e66a050375f5deea7d1e7985ebb41bfe84b9b
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-12-03 23:01:14 +0530

    Update codeforces117.java
    Added a comment to indicate the problem number for clarity.

--- codeforces117.java
+++ codeforces117.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class codeforces117
 {
-    public static void main(String args[])
+    public static void main(String args[])           //Problem 1729A - Two Elevators
     {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
@@ -27,4 +27,5 @@ public class codeforces117
                 System.out.println("3");
         }
     }
+
 }
