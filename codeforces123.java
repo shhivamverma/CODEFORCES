b7c4b2684d1fd80803187b14b5088097fc88219e
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-12-09 16:43:21 +0530

    Update codeforces123.java

--- codeforces123.java
+++ codeforces123.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class codeforces123
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)                // Problem 80A. Panoramix's Prediction
     {
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
@@ -27,3 +27,4 @@ public class codeforces123
             System.out.println("NO");
     }
 }
+
