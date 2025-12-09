7e7f1919362b2a45f2d880da3b0944c8943d2ac2
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-12-09 16:42:34 +0530

    Add files via upload

---
+++ codeforces123.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class codeforces123
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int next=n+1;
+        while(true) 
+        {
+            boolean prime=true;
+            for(int i=2;i*i<=next;i++) 
+            {
+                if(next % i == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime) break;
+            next++;
+        }
+        if (next == m)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
