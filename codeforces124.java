0d42ff2933d563b6d070d5ecb83d788a9b240e7d
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-12-10 19:13:34 +0530

    Add files via upload

---
+++ codeforces124.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class codeforces124 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if (a == b) {
+                System.out.println(c);
+            } else if (a == c) {
+                System.out.println(b);
+            } else {
+                System.out.println(a);
+            }
+        }
+
+    
+    }
+}
