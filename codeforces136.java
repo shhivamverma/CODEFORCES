34da13791fd5a73a53cd5102f8ae1f0abe6c443f
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-12-20 17:07:22 +0530

    Update codeforces136.java
    Updated code formatting and added a comment for clarity.

--- codeforces136.java
+++ codeforces136.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class codeforces136
 {
-    public static void main(String args[])
+    public static void main(String args[])                       // Problem 1883B. Chemistry
     {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
@@ -31,4 +31,5 @@ public class codeforces136
         }
     }
 }
+
 
