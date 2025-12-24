8c2a52d90e4d07e5339de4e846b8bd0fc32711aa
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-12-24 20:55:10 +0530

    Add files via upload

---
+++ codeforces142.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class codeforces142 
+{
+	public static void main(String args[]) 
+    {
+		long a = 1;
+		for (long i = 2; i <= 50; i++) 
+        {
+			a = lcm(a, i);
+		}
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int j=1;j<=t;j++)
+        {
+			long n=sc.nextLong();
+			int i=1;
+			while(n % i == 0) 
+            { 
+				i++;
+			}
+			System.out.println(i - 1);
+		}
+	}
+	
+	private static long gcd(long a, long b) {
+		while (b != 0) {
+			long temp = b;
+			b = a % b;
+			a = temp;
+		}
+		return a;
+	}
+    private static long lcm(long a, long b) {
+		return a * (b / gcd(a, b));
+	}
+}
