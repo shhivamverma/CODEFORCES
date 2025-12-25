9cf796c8348566a281a087aee23b6376413774b6
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-12-25 13:44:12 +0530

    Add files via upload

---
+++ codeforces144.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class codeforces144
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=1;i<=t;i++)
+        {
+            int n=sc.nextInt();
+            String s=sc.next();
+            int curr=1;
+            int large=1;
+            for(int j=1;j<n;j++)
+            {
+                if(s.charAt(j)==s.charAt(j-1))
+                {
+                    curr++;
+                }
+                else
+                {
+                    large=Math.max(large,curr);
+                    curr=1;
+                }
+            }
+            large=Math.max(curr,large);
+            System.out.println(large+1);
+        }
+    }
+}
