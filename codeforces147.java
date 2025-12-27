eb6c6300cfded43428d09b754392ba731eb98dc9
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-12-28 00:13:23 +0530

    Add files via upload

---
+++ codeforces147.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class codeforces147
+{
+	public static void main(String args[]) 
+    {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt(); 
+		 for(int j=1;j<=t;j++)
+         {
+			int n=sc.nextInt();  
+			int arr[]=new int[n];
+			for(int i=0;i<n;i++) 
+            {
+				arr[i]=sc.nextInt(); 
+            }
+			for(int i = 0; i < n; i++) 
+            {
+				if(arr[i] == 1) 
+                {
+					arr[i]++; 
+				}
+			}
+			for (int i=0;i<n-1;i++) 
+            {
+				if ((arr[i+1] % arr[i]) == 0) 
+                {
+					arr[i+1]++; 
+				}
+			}
+			for (int i=0;i<n;i++) 
+            {
+				System.out.print(arr[i]+" ");
+			}
+			System.out.println();
+		}
+    }
+}
