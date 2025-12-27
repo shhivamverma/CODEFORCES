b6bdc610b69202fc211517b9749e577b38adc855
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-12-28 00:12:18 +0530

    Update codeforces148.java
    Added a comment indicating the problem number for clarity.

--- codeforces148.java
+++ codeforces148.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class codeforces148
 {
-    public static void main(String args[])
+    public static void main(String args[])             // Problem 1726A - Mainak and Array
     {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
@@ -29,4 +29,5 @@ public class codeforces148
             System.out.println(ans);
         }
     }
+
 }
