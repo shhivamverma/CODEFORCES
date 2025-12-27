c85113282bafae9cf96251c8b62165cdab5191fc
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-12-28 00:15:28 +0530

    Update codeforces147.java
    Updated code formatting and added a comment for clarity.

--- codeforces147.java
+++ codeforces147.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class codeforces147
 {
-	public static void main(String args[]) 
+	public static void main(String args[])               // Problem 1794B. Not Dividing
     {
 		Scanner sc=new Scanner(System.in);
 		int t=sc.nextInt(); 
@@ -34,4 +34,5 @@ public class codeforces147
 			System.out.println();
 		}
     }
+
 }
