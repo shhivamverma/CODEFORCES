9d91c09026043eac2b46f784f0ec3619ebee1e03
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-12-28 13:14:48 +0530

    Update codeforces150.java
    Added a comment to specify the problem number for clarity.

--- codeforces150.java
+++ codeforces150.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class codeforces150
 {
-    public static void main(String args[])
+    public static void main(String args[])             // Problem 1679A. AvtoBus
 {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
@@ -21,3 +21,4 @@ public class codeforces150
         }
     }
 }
+
