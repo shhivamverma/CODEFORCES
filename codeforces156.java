b3e1c87f62e426635e3ece6c316d2a3c9e651104
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-12-30 00:34:51 +0530

    Add files via upload

---
+++ codeforces156.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class codeforces156
+{
+	public static void main(String args[]) 
+    {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt(); 
+		for(int i=1;i<=t;i++) 
+        {
+			long a = sc.nextLong(); 
+			long b = sc.nextLong(); 
+			long c = sc.nextLong(); 
+			int k=0;
+			long aa = (2 * b) - c; 
+			if (aa / a > 0 && aa % a == 0)
+            {
+                k=1;
+			}
+			long bb = (a + c) / 2; 
+			if (bb / b > 0 && bb % b == 0 && (c - a) % 2 == 0) 
+            { 
+				k=1;
+			}
+			long cc = 2 * b - a;
+			if (cc / c > 0 && cc % c == 0) 
+            { 
+                k=1;
+            }
+			if(k==1) 
+            {
+				System.out.println("YES");
+			} 
+            else 
+            {
+				System.out.println("NO");
+			}
+		}
+	}
+}
+
