e3036bfa03310a00dbf536dd905a44c0a8039eaa
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-12-30 00:40:03 +0530

    Update codeforces155.java
    Added a comment to specify the problem associated with the code.

--- codeforces155.java
+++ codeforces155.java
@@ -1,7 +1,7 @@
 import  java.util.*;
 public class codeforces155
 {
-    public static void main(String args[])
+    public static void main(String args[])                  // Problem 1665B. Array Cloning Technique
     {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
@@ -52,3 +52,4 @@ public class codeforces155
         }
     }
 }
+
