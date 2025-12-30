3080de13b3935cb20af9ac46ee06702c887ef965
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-12-31 03:37:32 +0530

    Update codeforces159.java
    Added a comment indicating the problem statement for clarity.

--- codeforces159.java
+++ codeforces159.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class codeforces159 
 {
-    public static void main(String args[]) 
+    public static void main(String args[])                 // Problem 1593B. Make it Divisible by 25
     {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
@@ -78,3 +78,4 @@ public class codeforces159
         }
     }
 }
+
