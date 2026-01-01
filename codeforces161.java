28438fd73eee25b2afdb818ce403694da2482359
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-01-01 13:39:29 +0530

    Add files via upload

---
+++ codeforces161.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class codeforces161 
+{
+	public static void main(String args[]) 
+    {
+		Scanner sc=new Scanner(System.in);
+		long t=sc.nextLong(); 
+		for(int j=1;j<=t;j++)
+        {
+			long n=sc.nextLong(); 
+			long a[]=new long[(int) n];
+			for (int i=0;i<n;i++) 
+            { 
+				a[i]=sc.nextLong();
+			}
+			long totalAnd=a[0]; 
+			for (int i=1;i<n;i++) 
+            { 
+				totalAnd = totalAnd & a[i];
+			}
+			System.out.println(totalAnd); 
+		}
+	}
+}
