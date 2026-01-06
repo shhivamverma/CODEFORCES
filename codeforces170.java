3ee5642d102625a823230ef630e254582f2b9279
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-01-06 12:00:26 +0530

    Update codeforces170.java

--- codeforces170.java
+++ codeforces170.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class codeforces170 
 {
-    public static void main(String args[]) 
+    public static void main(String args[])              // Problem 1798D. Shocking Arrangement
     {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
@@ -63,3 +63,4 @@ public class codeforces170
         }
     }
 }
+
