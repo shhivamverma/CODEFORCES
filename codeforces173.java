032fc09b66f8fc2e71b176be9cfa278fdabb20dc
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-01-07 12:10:02 +0530

    Add files via upload

---
+++ codeforces173.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class codeforces173
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=1;i<=t;i++)
+        {
+            String str=sc.next();
+            int c1=0;
+            int c2=0;
+            for(int j=0;j<str.length();j++)
+            {
+                if(str.charAt(j)=='0')
+                {
+                    c1++;
+                }
+                else
+                {
+                    c2++;
+                }
+            }
+            int ops=(int)Math.min(c1,c2);
+            if(ops%2 != 0)
+            {
+                System.out.println("DA");
+            }
+            else
+            {
+                System.out.println("NET");
+            }
+        }
+    }
+}
