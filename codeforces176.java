7d70c5f8e604464d16ddca37b7982d62f432d95c
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-01-08 10:14:02 +0530

    Update codeforces176.java

--- codeforces176.java
+++ codeforces176.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class codeforces176
 {
-    public static void main(String args[])
+    public static void main(String args[])                   // Problem 1913B. Swap and Delete
     {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
@@ -43,4 +43,5 @@ public class codeforces176
             System.out.println(n-lt);
         }
     }
+
 }
