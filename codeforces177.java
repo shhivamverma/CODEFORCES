1a92d7ea0eee3752681067f3e8c6bfee893a2e3b
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-01-08 23:43:06 +0530

    Add files via upload

---
+++ codeforces177.java
@@ -0,0 +1,54 @@
+import java.util.*;
+public class codeforces177
+{
+	public static void main(String args[]) 
+    {
+		Scanner sc=new Scanner(System.in);
+		long t=sc.nextLong(); 
+		for(int j=1;j<=t;j++)
+        {
+			long n=sc.nextLong(); 
+			long k=sc.nextLong(); 
+			long a[]=new long[(int) n];
+			for (int i=0;i<n;i++) 
+            {
+				a[i]=sc.nextLong();
+			}
+			long ans=Integer.MAX_VALUE; 
+			long ec=0; 
+			for (int i=0;i<n;i++) 
+            {
+				if (a[i]%2==0)
+                {
+					ec++; 
+				}
+				if (a[i]%k==0) 
+                {
+					ans = 0; 
+				}
+                else
+                {  
+				    ans = Math.min(ans, (k-a[i]%k));
+                }
+			}
+			if (k==4) 
+            {
+                int m;
+				if (ec>=2) 
+                {
+					m=0; 
+				} 
+                else if (ec==1)
+                {
+					m=1; 
+				} 
+                else
+                {
+					m=2; 
+             	}
+                ans=(Math.min(ans,(long)m)); 
+			}
+			System.out.println(ans); 
+		}
+	}
+}
