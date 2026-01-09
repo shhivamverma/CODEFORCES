5f07c808e3e9b35a1d8dec08132550ed11f37eeb
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-01-10 02:36:49 +0530

    Add files via upload

---
+++ codeforces180.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class codeforces180
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0) 
+        {
+            int n=sc.nextInt();
+            long b[]=new long[n];
+            long first=Long.MAX_VALUE;
+            for (int i=0;i<n;i++) 
+            {
+                int m=sc.nextInt();
+                long a[]=new long[m];
+                for(int j=0;j<m;j++)
+                {
+                    a[j]=sc.nextLong();
+                }
+                Arrays.sort(a);
+                b[i] = a[1];
+                if(a[0]<first)
+                {
+                    first=a[0];
+                }
+            }
+            Arrays.sort(b);
+            long sum=0;
+            for (int i=0;i<n;i++) 
+            {
+                sum=sum+b[i];
+            }
+            long ans=first+sum-b[0];
+            System.out.println(ans);
+        }
+    }
+}
