eb144b80962400922229b1c862f33f321ef1147e
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-01-13 15:47:41 +0530

    Add files via upload

---
+++ codeforces188.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class codeforces188
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long n = sc.nextLong();
+            long k = sc.nextLong();
+
+            boolean found = false;
+
+            for (int i = 0; i < 32; i++) {
+                long p = (long) Math.pow(2, i);
+                long q = n / p;
+                long r = n % p;
+
+                if (k == q + 1 && r > 0) {
+                    System.out.println(i);
+                    found = true;
+                    break;
+                }
+
+                if (k == q && p > r) {
+                    System.out.println(i);
+                    found = true;
+                    break;
+                }
+
+                if (q + 1 < k) break;
+            }
+
+            if (!found) {
+                System.out.println(-1);
+            }
+        }
+
+    }
+}
