4209ba2a122613966cc403b9bbd46e92d50a5d88
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-01-14 11:43:53 +0530

    Update codeforces190.java
    Added a comment to specify the problem number for clarity.

--- codeforces190.java
+++ codeforces190.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class codeforces190
 {
-    public static void main(String args[])
+    public static void main(String args[])                  // Problem 1744C. Traffic Light
     {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
@@ -29,4 +29,5 @@ public class codeforces190
             System.out.println(max);
         }
     }
+
 }
