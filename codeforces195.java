dca70218ba059a3138777963bc621eb8e889043a
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-01-16 16:38:31 +0530

    Update codeforces195.java
    Added a comment indicating the problem number for clarity.

--- codeforces195.java
+++ codeforces195.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class codeforces195
 {
-    public static void main(String args[]) 
+    public static void main(String args[])         // Problem 976C. Nested Segments
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
@@ -37,3 +37,4 @@ public class codeforces195
         System.out.println("-1 -1");
     }
 }
+
