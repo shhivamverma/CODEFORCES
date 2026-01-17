90f450e3030a92f50470468a60f23e9ef2511b75
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-01-17 17:18:29 +0530

    Add files via upload

---
+++ codeforces198.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class codeforces198
+{
+	public static void main(String args[]) 
+    {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0) 
+        {
+			long w=sc.nextLong(); 
+			long h=sc.nextLong();
+			long area=Long.MIN_VALUE; 
+			for (int i=0;i<4;i++) 
+            {
+				long k=sc.nextLong(); 
+				long first=0;
+                long last=0;
+				for (int j=0;j<k;j++) 
+                { 
+					long x=sc.nextLong(); 
+					if (j == 0)
+                    {
+						first = x; 
+					}
+					if (j == k - 1) 
+                    {
+						last = x; 
+					}
+				}
+				long base = last - first; 
+				long height;
+				if (i < 2) 
+                {
+					height = h; 
+				} 
+                else 
+                {
+					height = w;
+				}
+				area = Math.max(area, base*height); 
+			}
+			System.out.println(area); 
+		}
+		
+	}
+}
