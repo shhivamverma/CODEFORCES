b81d80a61fb789019ca4c0af056fc0d2c02b5e75
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-01-22 20:15:41 +0530

    Add files via upload

---
+++ codeforces206.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class codeforces206
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        long m=sc.nextLong();
+        long a[]=new long[(int)n];
+        for(int i=0;i<n;i++) 
+        {
+            a[i]=sc.nextLong();
+        }
+        if(n>m) 
+        {
+            System.out.println(0);
+            return;
+        }
+        long prod=1;
+        for(int i=0;i<n-1;i++) 
+        {
+            for(int j=i+1;j<n;j++) 
+            {
+                prod=(prod*Math.abs(a[i]-a[j]))%m;
+            }
+        }
+        System.out.println(prod%m);
+    }
+}
