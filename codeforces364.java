7bfb5ab7aab3e821c680b18c738f6b54b97688f7
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-05-23 23:08:43 +0530

    Update codeforces364.java

--- codeforces364.java
+++ codeforces364.java
@@ -1,19 +1,47 @@
 // Once I am done , I will be gone , You won't see me for a while.
 
 import java.util.*;
-public class codeforces364
+public class codeforces363
 {
-    public static void main(String args[]) 
+    public static void main(String args[])                      // Problem 2044C. Hard Problem
     {
-        Scanner sc =new Scanner(System.in);
-        int t = sc.nextInt();
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
         while(t-->0) 
         {
-            int x=sc.nextInt();
-            int y=sc.nextInt();
-            int mini = Math.min(x, y);
-            int maxi = Math.max(x, y);
-            System.out.println(mini + " " + maxi);
+            int m=sc.nextInt();
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            int sum=0;
+            int n=0;
+            if(m>a) 
+            {
+                sum=sum+a;
+                n=n+(m-a);
+            } 
+            else 
+            {
+                sum=sum+m;
+            }
+            if(m>b) 
+            {
+                sum=sum+b;
+                n=n+(m-b);
+            } 
+            else 
+            {
+                sum=sum+m;
+            }
+            if(c<n) 
+            {
+                sum=sum+c;
+            } 
+            else 
+            {
+                sum=sum+n;
+            }
+            System.out.println(sum);
         }
     }
 }
