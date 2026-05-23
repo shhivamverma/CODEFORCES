1e0994e723c6959f413e5badeaaaca03e980bf15
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-05-23 23:11:42 +0530

    Update codeforces363.java

--- codeforces363.java
+++ codeforces363.java
@@ -1,45 +1,29 @@
 // Once I am done , I will be gone , You won't see me for a while.
-
 import java.util.*;
-public class codeforces363
+public class codeforces362
 {
     public static void main(String args[]) 
-    {
+    {                                                                  // Problem 1676H1. Maximum Crossings (Easy Version)
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
         while(t-->0) 
         {
-            int m=sc.nextInt();
-            int a=sc.nextInt();
-            int b=sc.nextInt();
-            int c=sc.nextInt();
-            int sum=0;
-            int n=0;
-            if(m>a) 
+            long n=sc.nextLong();
+            long a[]=new long[(int)n];
+            for(int i=0;i<n;i++) 
             {
-                sum=sum+a;
-                n=n+(m-a);
-            } 
-            else 
-            {
-                sum=sum+m;
+                a[i]=sc.nextLong();
             }
-            if(m>b) 
+            long sum=0;
+            for(int i=0;i<n-1;i++) 
             {
-                sum=sum+b;
-                n=n+(m-b);
-            } 
-            else 
-            {
-                sum=sum+m;
-            }
-            if(c<n) 
-            {
-                sum=sum+c;
-            } 
-            else 
-            {
-                sum=sum+n;
+                for(int j=i+1;j<n;j++) 
+                {
+                    if(a[i]>=a[j]) 
+                    {
+                        sum++;
+                    }
+                }
             }
             System.out.println(sum);
         }
