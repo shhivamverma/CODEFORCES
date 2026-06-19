9d6775c363bb60e423df91b54d5ce737ad8ddc03
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-06-19 18:02:17 +0530

    Update codeforces405.java
    Updated comments and added a newline at the end of the file.

--- codeforces405.java
+++ codeforces405.java
@@ -3,7 +3,7 @@
 import java.util.*;
 public class codeforces405
 {
-    public static void main(String args[])
+    public static void main(String args[])                                    // Problem 2237A. Destroying Towers
     {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
