6074b43bf860c634becbf8c19d4d1ebe90d529f7
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-06-21 21:07:55 +0530

    Update codeforces408.java
    Updated the comment to specify the problem statement.

--- codeforces408.java
+++ codeforces408.java
@@ -3,7 +3,7 @@
 import java.util.*;
 public class codeforces408
 {
-    public static void main(String args[]) 
+    public static void main(String args[])                           // Problem - 2237D. Fullmetal Bitchemist 
     {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
