57f8461a392996b743545dd60ca6f6451a714da7
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-06-24 11:27:08 +0530

    Update codeforces411.java
    The comment at the top was retained, and a note about the problem was added to the main method.

--- codeforces411.java
+++ codeforces411.java
@@ -3,7 +3,7 @@
 import java.util.*;
 public class codeforces411
 {
-    public static void main(String args[]) 
+    public static void main(String args[])                      // Problem 1537E2. Erase and Extend (Hard Version)
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
