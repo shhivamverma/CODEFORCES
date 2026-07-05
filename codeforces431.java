c311e2dba64a3c134c7bb52fff8c723521a1c685
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-07-06 04:58:22 +0530

    Update codeforces431.java
    Restored commented header and problem statement.

--- codeforces431.java
+++ codeforces431.java
@@ -10,7 +10,7 @@
 import java.util.*;
 public class codeforces431
 {
-    public static void main(String args[]) 
+    public static void main(String args[])                                     // Problem - 584A. Olesya and Rodion
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
