467faa6547f7e80f3fa5b1939f6a1d7c1115c04c
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-07-06 04:52:13 +0530

    Add files via upload

---
+++ codeforces430.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class codeforces430
+{
+    static final long MOD=1000000007L;
+    static long power(long base,long x) 
+    {
+        long ans=1;
+        base=base%MOD;
+        while(x>0) 
+        {
+            if(x%2==0) 
+            {
+                base=(base*base)%MOD;
+                x=x/2;
+            }
+            else 
+            {
+                ans=(ans*base)%MOD;
+                x--;
+            }
+        }
+            return ans;
+    }
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        long anss=(power(3,3*n)-power(7,n)+MOD)%MOD;
+        System.out.println(anss);
+    }
+}
