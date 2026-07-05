e41db24a6da4b9d1a9b6212e1b11bf3ae677cf37
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-07-06 04:53:13 +0530

    Add files via upload

---
+++ codeforces431.java
@@ -0,0 +1,41 @@
+// Once I am done , I will be gone , You won't see me for a while.
+
+//  ███████╗ ██╗  ██╗ ██╗ ██╗   ██╗  █████╗  ███╗   ███╗
+//  ██╔════╝ ██║  ██║ ██║ ██║   ██║ ██╔══██╗ ████╗ ████║
+//  ███████╗ ███████║ ██║ ██║   ██║ ███████║ ██╔████╔██║
+//  ╚════██║ ██╔══██║ ██║ ╚██╗ ██╔╝ ██╔══██║ ██║╚██╔╝██║
+//  ███████║ ██║  ██║ ██║  ╚████╔╝  ██║  ██║ ██║ ╚═╝ ██║
+//  ╚══════╝ ╚═╝  ╚═╝ ╚═╝   ╚═══╝   ╚═╝  ╚═╝ ╚═╝     ╚═╝
+
+import java.util.*;
+public class codeforces431
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int t=sc.nextInt();
+        if(t==10) 
+        {
+            if(n==1) 
+            {
+                System.out.println(-1);
+            } 
+            else 
+            {
+                System.out.print(1);
+                for(int i=1;i<n;i++) 
+                {
+                    System.out.print(0);
+                }
+            }
+        } 
+        else 
+        {
+            for(int i=0;i<n;i++) 
+            {
+                System.out.print(t);
+            }
+        }
+    }
+}
