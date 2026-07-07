3d8258a0a59884bd499fe5b9b47a94e11be69e92
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-07-07 21:14:17 +0530

    Add files via upload

---
+++ codeforces436.java
@@ -0,0 +1,43 @@
+// Once I am done , I will be gone , You won't see me for a while.
+
+//  ███████╗ ██╗  ██╗ ██╗ ██╗   ██╗  █████╗  ███╗   ███╗
+//  ██╔════╝ ██║  ██║ ██║ ██║   ██║ ██╔══██╗ ████╗ ████║
+//  ███████╗ ███████║ ██║ ██║   ██║ ███████║ ██╔████╔██║
+//  ╚════██║ ██╔══██║ ██║ ╚██╗ ██╔╝ ██╔══██║ ██║╚██╔╝██║
+//  ███████║ ██║  ██║ ██║  ╚████╔╝  ██║  ██║ ██║ ╚═╝ ██║
+//  ╚══════╝ ╚═╝  ╚═╝ ╚═╝   ╚═══╝   ╚═╝  ╚═╝ ╚═╝     ╚═╝
+
+import java.util.*;
+public class codeforces436
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0) 
+        {
+            int n=sc.nextInt();
+            HashMap<Long,Long> mp=new HashMap<>();
+            for (int i=0;i<n;i++) 
+            {
+                long x=sc.nextLong();
+                long freq=mp.getOrDefault(x,0L)+1;
+                mp.put(x,freq);
+            }
+            ArrayList<Long> list=new ArrayList<>(mp.keySet());
+            Collections.sort(list);
+            long ans=0;
+            for(int i=0;i<list.size();i++) 
+            {
+                long x=list.get(i);
+                long cur=mp.get(x);
+                long prev=mp.getOrDefault(x-1,0L);
+                if(cur>prev) 
+                {
+                    ans=ans+(cur-prev);
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+}
