523d5b57c720b832def6464b30442997bb703fc8
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-07-22 18:56:38 +0530

    Add files via upload

---
+++ codeforces454.java
@@ -0,0 +1,50 @@
+// Once I am done , I will be gone , You won't see me for a while.
+
+//  ███████╗ ██╗  ██╗ ██╗ ██╗   ██╗  █████╗  ███╗   ███╗
+//  ██╔════╝ ██║  ██║ ██║ ██║   ██║ ██╔══██╗ ████╗ ████║
+//  ███████╗ ███████║ ██║ ██║   ██║ ███████║ ██╔████╔██║
+//  ╚════██║ ██╔══██║ ██║ ╚██╗ ██╔╝ ██╔══██║ ██║╚██╔╝██║
+//  ███████║ ██║  ██║ ██║  ╚████╔╝  ██║  ██║ ██║ ╚═╝ ██║
+//  ╚══════╝ ╚═╝  ╚═╝ ╚═╝   ╚═══╝   ╚═╝  ╚═╝ ╚═╝     ╚═╝
+
+import java.util.*;
+public class codeforces454
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            ArrayList<Long> a=new ArrayList<>();
+            long x;
+            int curr=0;
+            for(int i=0;i<n;i++)
+            {
+                x=sc.nextLong();
+                a.add(x);
+            }      
+            if(n==1)
+            {
+                System.out.println(1);
+                continue;
+            }
+            int count=0;
+            for(int i=1;i<n;i++)
+            {
+                if(a.get(i-1)>a.get(i) && curr!=1)
+                {
+                    count++;
+                    curr=1;
+                }
+                else if(a.get(i-1)<a.get(i) && curr!=-1)
+                {
+                    count++;
+                    curr=-1;
+                }
+            }
+            System.out.println(count+1);
+        }
+    }
+}
