7e8e8ed9191662e79f581ff2c80320cb22e2106d
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-08-02 22:17:53 +0530

    Add files via upload

---
+++ codeforces466.java
@@ -0,0 +1,54 @@
+// Once I am done , I will be gone , You won't see me for a while.
+
+//  ███████╗ ██╗  ██╗ ██╗ ██╗   ██╗  █████╗  ███╗   ███╗
+//  ██╔════╝ ██║  ██║ ██║ ██║   ██║ ██╔══██╗ ████╗ ████║
+//  ███████╗ ███████║ ██║ ██║   ██║ ███████║ ██╔████╔██║
+//  ╚════██║ ██╔══██║ ██║ ╚██╗ ██╔╝ ██╔══██║ ██║╚██╔╝██║
+//  ███████║ ██║  ██║ ██║  ╚████╔╝  ██║  ██║ ██║ ╚═╝ ██║
+//  ╚══════╝ ╚═╝  ╚═╝ ╚═╝   ╚═══╝   ╚═╝  ╚═╝ ╚═╝     ╚═╝
+
+// LinkedIn - https://www.linkedin.com/in/shivamverma-dev
+// GitHub - https://github.com/shhivamverma
+// Portfolio - https://my-next-portfolio-hlye.vercel.app/
+
+import java.util.*;
+public class codeforces466
+{
+	public static void main(String args[]) 
+    {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt(); 
+		while(t-->0) 
+        {
+			int n=sc.nextInt(); 
+			int a[]=new int[n];
+			int b[]=new int[n];
+			int diff[]=new int[n]; 
+			int max=Integer.MIN_VALUE; 
+			for(int i=0;i<n;i++) 
+            {
+				a[i]=sc.nextInt();
+			}
+			for(int i=0;i<n;i++) 
+            {
+				b[i]=sc.nextInt();
+				diff[i]=a[i]-b[i]; 
+				max=Math.max(max,diff[i]); 
+			}
+			List<Integer> ans=new ArrayList<>();
+			for(int i=0;i<n;i++) 
+            {
+				if (diff[i]==max) 
+                {
+					ans.add(i+1); 
+				}
+			}
+			System.out.println(ans.size());
+			for(int j=0;j<ans.size();j++)
+            {
+				System.out.print(ans.get(j)+" "); 
+			}
+			System.out.println();
+		}
+	}
+}
